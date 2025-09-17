acfd3ba017d8bb6726ef10d183587e5ec0b4a7d8
Rubai Rahman <rubairahman1@gmail.com>
2025-09-17 23:26:16 +0600

    pulseEffect added

--- src/components/pulsatingEffect.tsx
+++ src/components/pulsatingEffect.tsx
@@ -5,9 +5,14 @@ import { motion } from 'motion/react';
 const PulsatingEffect = () => {
   return (
     <motion.button
-      initial={{ scale: [1, 1.1, 1] }}
+      // initial={{ scale: [1, 1.1, 1] }}
       animate={{ scale: [1, 1.1, 1] }}
-      transition={{ duration: 1, ease: 'easeInOut', repeat: Infinity }}
+      transition={{
+        duration: 1,
+        ease: 'easeInOut',
+        repeat: Infinity,
+        repeatType: 'loop',
+      }}
       className="bg-blue-500 text-white px-4 py-2 rounded"
     >
       Pulsating Button
